12eee875589c5993b7cc7475d6beebb7d579a255
RicardoGoncal <ricardo.studo@gmail.com>
2025-02-03 15:01:24 -0300

    ajustes depoy github actions

--- front-end/my-vue-app/src/config.js
+++ front-end/my-vue-app/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL_OMDB = "http://34.70.150.73/api1";
-export const API_URL_POKE = "http://34.70.150.73/api2";
+export const API_URL_OMDB = "http://10.96.133.64:30000/api1";
+export const API_URL_POKE = "http://10.96.163.26:30001/api2";
