9cf5260c96e2d17d1d4db3eccf4e0d19726a2206
RicardoGoncal <ricardo.studo@gmail.com>
2025-03-25 09:39:56 -0300

    feat: update deployments github - update templates about frontend

--- front-end/my-vue-app/src/config.js
+++ front-end/my-vue-app/src/config.js
@@ -1,8 +1,8 @@
 // Export para uso de Nodeport
-export const API_URL_OMDB = "http://localhost:30000/api1";
-export const API_URL_POKE = "http://localhost:30001/api2";
+// export const API_URL_OMDB = "http://localhost:30000/api1";
+// export const API_URL_POKE = "http://localhost:30001/api2";
 
 
 // Export para uso de LoadBalancer
-// export const API_URL_OMDB = "http://localhost:30000/api1";
-// export const API_URL_POKE = "http://localhost:30001/api2";
+export const API_URL_OMDB = "http://http://35.238.212.27/api1";
+export const API_URL_POKE = "http://http://35.238.212.27/api2";
